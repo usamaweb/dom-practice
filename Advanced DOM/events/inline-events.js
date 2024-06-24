fd4790df15e95ab0bbbc1baa7ec7e7c32f0f16d7
Usama Umar <usamathedev@gmail.com>
2024-06-24 13:57:38 +0500

    inline events test

---
+++ Advanced DOM/events/inline-events.js
@@ -0,0 +1,15 @@
+const title = document.querySelector('#title')
+const firstPara = document.querySelector('#first-paragraph')
+const secondPara = document.querySelector('#second-paragraph')
+
+function colorBlue() {
+    title.style.color = 'blue'
+    firstPara.style.color = 'blue'
+    secondPara.style.color = 'blue'
+}
+
+function colorRed() {
+    title.style.color = 'red'
+    firstPara.style.color = 'red'
+    secondPara.style.color = 'red'
+}
